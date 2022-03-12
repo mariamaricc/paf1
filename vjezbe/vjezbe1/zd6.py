9ce1024392df42ce6e86205c57a559303f0604ae
mariamaricc <100929156+mariamaricc@users.noreply.github.com>
2022-03-12 01:06:36 +0100

    Update zd6.py

--- vjezbe/vjezbe1/zd6.py
+++ vjezbe/vjezbe1/zd6.py
@@ -1,8 +1,9 @@
 import math
 from decimal import Decimal
+from textwrap import fill
 import matplotlib.pyplot as plt
 import numpy as np
-
+ax = plt.subplot()
 
 def kruznica():
     p = float(input("unesite x koordinatu sredista: "))
@@ -17,8 +18,21 @@ def kruznica():
     if r == r1:
         print("tocka lezi na kruznici")
     elif r < r1:
-        print("tocka lezi unutar kruznice i udaljena je od nje za {}".format())
+        d = r1 - r
+        print("tocka lezi unutar kruznice i udaljena je od nje za {}".format(d))
     else:
-        print("tocka lezi izvan kruznice")
+        d = r - r1
+        print("tocka lezi izvan kruznice i udaljena je od nje za {}".format)
+        
+    krug = plt.Circle((p,q),r, fill=False)
+    ax.scatter(x, y, s=r)
+    ax.add_patch(krug)
+    plt.axis('square')
+    pohrana = input("1. prikaz slike na ekranu ili 2. spremanje kao pdf? ")
+    if pohrana == "1":
+        plt.show()
+    else:
+        spremanje = input("pod kojim imenom zelite spremiti file? ")
+        plt.savefig("{}.pdf".format(spremanje))
    
 kruznica()
